535b8cf3e2774394d279d545bdcc2e6d3f5feddd
behhanyu <hbeh0003@student.monash.edu>
2025-02-16 16:28:24 +0800

    delete unused backend folder

--- pms-backend/src/main/java/com/pms/pms_backend/repository/ParcelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pms.pms_backend.repository;
-
-import com.pms.pms_backend.entity.Parcel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParcelRepository extends JpaRepository<Parcel, Long> {
-}
